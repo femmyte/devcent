edd861891d4cf0b0a53b1135b519e5fb1b16cc9d
femmyte <sanyaoluadefemi@gmail.com>
2023-03-17 10:54:00 +0100

    Created a Grid component: This is used to creat a Grid system instead of using it one by one

---
+++ components/Grid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Grid = ({colNum, children}) => {
+  return (
+      <div className={`grid grid-cols-1 md:grid-cols-${colNum ? colNum : '3'} gap-x-[100px] mb-[20px]`}>
+          {children}
+          </div>
+  )
+}
+
+export default Grid
