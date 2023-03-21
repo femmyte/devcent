f806fe912107eca295b1b8d03980a97e7f55433a
femmyte <sanyaoluadefemi@gmail.com>
2023-03-21 23:04:06 +0100

    Created the auth api for the next-auth

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions = {
+  // Configure one or more authentication providers
+    providers: [
+         GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        CredentialsProvider({
+      //   name: 'Credentials',
+      //       credentials: {
+      //        email: { label: "Email", type: "text", placeholder: "Email" },
+      // password: { label: "Password", type: "password" }
+      //   },
+            
+            
+    async authorize(credentials, req) {
+    //   // Add logic here to look up the user from the credentials supplied
+        const { email, password } = credentials
+        
+        const res = await fetch(`https://bluealgorithm-api-production.up.railway.app/v1/admin/login`, {
+            method: 'POST',
+            headers: { "Content-Type": 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+        const user = await res.json();
+
+        console.log(res);
+        if (res.ok && user) {
+            return user
+        } else return null
+    }
+  })
+    // ...add more providers here
+    ],
+    session: {
+        strategy: 'jwt'
+    },
+    // add the sign in page
+    pages: {
+        signIn: '/signin'
+    }
+}
+
+export default NextAuth(authOptions)
