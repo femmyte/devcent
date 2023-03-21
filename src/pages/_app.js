07650dc42dd627163497c81f9e136f0654d8f297
femmyte <sanyaoluadefemi@gmail.com>
2023-03-21 23:00:59 +0100

    Set up a sessionProvider to enable the use of session on the app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
-
+import { AuthContextProvider } from 'AuthContext'
+import {SessionProvider} from 'next-auth/react'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SessionProvider>
+    <AuthContextProvider>
+        <Component {...pageProps} />
+    </AuthContextProvider>
+    </SessionProvider>
+      )
 }
