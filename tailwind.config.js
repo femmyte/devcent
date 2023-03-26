282cfa3d4005b5385bed710bbfc5d9786e51ef6a
femmyte <sanyaoluadefemi@gmail.com>
2023-03-26 15:40:44 +0100

    Added new Dependencies needed in the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // plugins: [require("daisyui")],
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+	// plugins: [require("daisyui")],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primaryYellow: "#FFBA0E",
-        primaryPurple: "#E40084",
-      },
-      fontFamily: {
-        space: ["Space Grotesk"],
-        dmsans: ["DM Sans"],
-        kaushan: ["Kaushan Script"],
-        source: ["Source Sans Pro"],
-      },
-    },
-  },
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primaryYellow: '#FFBA0E',
+				primaryPurple: '#E40084',
+			},
+			fontFamily: {
+				space: ['Space Grotesk'],
+				dmsans: ['DM Sans'],
+				kaushan: ['Kaushan Script'],
+				source: ['Source Sans Pro'],
+			},
+			gridTemplateColumns: {
+				'1/5': '1fr 5fr',
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
 };
