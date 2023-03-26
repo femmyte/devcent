ab80625841b1d263a60d0b2c0dbe8cecf3e769c6
femmyte <sanyaoluadefemi@gmail.com>
2023-03-26 15:49:13 +0100

    Created the Day view component of the calendar for the schedular page This component shows the monthly view of the full calendar

---
+++ components/dashboard/schedular/Day.js
@@ -0,0 +1,65 @@
+import { useStateContext } from 'AuthContext';
+import dayjs from 'dayjs';
+import React, { useState, useEffect } from 'react';
+
+const Day = ({ day, rowIdx }) => {
+	const getCurentDayClass = () => {
+		return day.format('DD-MM-YY') === dayjs().format('DD-MM-YY')
+			? 'bg-blue-600 text-white rounded-full w-7 '
+			: '';
+	};
+	const [dayEvent, setDayEvent] = useState([]);
+	const {
+		setDaySelected,
+		daySelected,
+		savedEvents,
+		setShowEventModal,
+		filteredEvents,
+		setSelectedEvent,
+	} = useStateContext();
+	useEffect(() => {
+		const events = filteredEvents?.filter(
+			(evt) =>
+				dayjs(evt.day).format('DD-MM-YY') === day.format('DD-MM-YY')
+		);
+		setDayEvent(events);
+	}, [filteredEvents, day]);
+
+	return (
+		<div className='border border-gray-200 flex flex-col cursor-pointer'>
+			<header className='flex flex-col items-center'>
+				{rowIdx === 0 && (
+					<p className='text-sm mt-1'>
+						{day.format('dddd').toUpperCase()}
+					</p>
+				)}
+				<p
+					className={`text-sm p-1 my-1 text-center ${getCurentDayClass()} `}
+				>
+					{day.format('DD')}
+				</p>
+			</header>
+			<div
+				className='flex-1 cursor-pointer'
+				onClick={() => {
+					setShowEventModal(true);
+					setDaySelected(day);
+				}}
+			>
+				{dayEvent?.map((evt, idx) => (
+					<div
+						key={idx}
+						className={`bg-${evt.label}-200 w-full  text-center p-1 mr-3 text-white text-sm rounded mb-1 truncate`}
+						style={{ backgroundColor: evt.label }}
+						onClick={() => setSelectedEvent(evt)}
+					>
+						<p>{evt.title}</p>
+						{/* <p>{evt.description}</p> */}
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Day;
