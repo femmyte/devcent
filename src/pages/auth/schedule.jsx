89a64f9f0f4f87ecb72569f269cb6cb4adf3cc1e
femmyte <sanyaoluadefemi@gmail.com>
2023-03-26 15:41:32 +0100

    Created the Schedule page

---
+++ src/pages/auth/schedule.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { getMonth } from 'appData/calendarUtils';
+import DashboardLayout from 'components/dashboard/DashboardLayout';
+import CalendarHeader from 'components/dashboard/schedular/CalendarHeader';
+import Month from 'components/dashboard/schedular/Month';
+import Sidebar from 'components/dashboard/schedular/Sidebar';
+import { useStateContext } from 'AuthContext';
+import EventModal from 'components/dashboard/schedular/EventModal';
+const Schedular = () => {
+	const [currentMonth, setCurrentMonth] = useState(getMonth());
+	const { monthIndex, showEventModal, isReady } = useStateContext();
+	useEffect(() => {
+		setCurrentMonth(getMonth(monthIndex));
+	}, [monthIndex]);
+
+	if (isReady) {
+		return <p>Loading...</p>;
+	}
+	return (
+		<>
+			<DashboardLayout>
+				{isReady && <p>Loading...</p>}
+				{showEventModal && <EventModal />}
+				<div className='min-h-screen flex flex-col text-white w-full px-2'>
+					<h1 className='font-[700] text-white text-[20px] md:text-[24px] leading-[30.24px] '>
+						Schedule
+					</h1>
+					<p className='font-plus font-[500] text-[16px] text-[#71717a] leading-[20.16px] mb-[20px] mt-[8px]'>
+						Your day to day activities
+					</p>
+					<CalendarHeader />
+					<div className='flex flex-1'>
+						{/* <Sidebar /> */}
+						<Month month={currentMonth} />
+					</div>
+				</div>
+			</DashboardLayout>
+		</>
+	);
+};
+
+export default Schedular;
