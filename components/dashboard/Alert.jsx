c0d1d3399cb0a0f6620aa5729bd4dea24d42f9bc
femmyte <sanyaoluadefemi@gmail.com>
2023-05-01 12:24:42 +0100

    Created an Aler component to hanldle the alert

---
+++ components/dashboard/Alert.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+// import { XIcon } from '@heroicons/react/outline';
+
+const Alert = ({ type, title, message }) => {
+	const [isOpen, setIsOpen] = useState(true);
+
+	return (
+		<>
+			{isOpen && (
+				<div
+					className={` w-full p-4 rounded-[4px] ${
+						isOpen
+							? 'transform translate-y-0 transition ease-out duration-300'
+							: 'transform -translate-y-full transition ease-in duration-200'
+					} `}
+					style={{ backgroundColor: 'rgba(241, 234, 238, 0.94)' }}
+				>
+					<div className='flex justify-between items-center'>
+						<div className='mr-3 flex gap-x-[20px] items-center'>
+							<img
+								src='/images/icons/markbg.png'
+								className='w-[20px] h-[20px] rounded-full'
+							/>
+							<div className=''>
+								<p className='font-[400] text-[14px] text-[#202223] leading-5'>
+									{title}
+								</p>
+								{message && (
+									<p className='mt-[10px] font-[500] text-[14px] text-[#202223] leading-5'>
+										{message}{' '}
+									</p>
+								)}
+							</div>
+						</div>
+						<div className='flex-shrink-0'>
+							{/* <XIcon
+								className='h-5 w-5 text-yellow-400'
+								onClick={() => setIsOpen(false)}
+							/> */}
+							<button
+								type='button'
+								className='mr-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-gray-800 dark:text-green-400 dark:hover:bg-gray-700 cursor-pointer'
+								data-dismiss-target='#alert-border-3'
+								aria-label='Close'
+								onClick={() => setIsOpen(!isOpen)}
+							>
+								<span className='sr-only'>Dismiss</span>
+								<svg
+									aria-hidden='true'
+									className='w-5 h-5'
+									fill='currentColor'
+									viewBox='0 0 20 20'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										fillRule='evenodd'
+										d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+										clipRule='evenodd'
+									></path>
+								</svg>
+							</button>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default Alert;
