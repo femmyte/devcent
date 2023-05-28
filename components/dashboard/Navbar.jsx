61307b32b87559fa1882d2c37dd239b906417fca
femmyte <sanyaoluadefemi@gmail.com>
2023-05-28 15:36:59 +0100

    Check if the user is loggedin

--- components/dashboard/Navbar.jsx
+++ components/dashboard/Navbar.jsx
@@ -1,152 +1,155 @@
-import React, { useState, useEffect } from "react";
-import { AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
-import { RiNotification3Line } from "react-icons/ri";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import { FaToggleOn, FaToggleOff } from "react-icons/fa";
-import { IoEyeSharp } from "react-icons/io5";
-// import { getUser, removeUserSession } from '../common'
-import Link from "next/link";
-import { useSession, signOut } from "next-auth/react";
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMail, AiOutlineMenu } from 'react-icons/ai';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { FaToggleOn, FaToggleOff } from 'react-icons/fa';
+import { IoEyeSharp } from 'react-icons/io5';
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
 // import { data } from "./Notification";
 
-import { useStateContext } from "../../AuthContext";
+import { useStateContext } from '../../AuthContext';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor, num }) => (
-  <div className="relative">
-    <button
-      type="button"
-      onClick={customFunc}
-      style={{ color }}
-      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
-    >
-      {icon}
-    </button>
-    <div
-      style={{ backgroundColor: dotColor }}
-      className="absolute rounded-full h-[15px] w-[15px] right-[7px] top-[4px] text-[10px] text-white flex items-center justify-center"
-    >
-      {num}
-    </div>
-  </div>
+	<div className='relative'>
+		<button
+			type='button'
+			onClick={customFunc}
+			style={{ color }}
+			className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+		>
+			{icon}
+		</button>
+		<div
+			style={{ backgroundColor: dotColor }}
+			className='absolute rounded-full h-[15px] w-[15px] right-[7px] top-[4px] text-[10px] text-white flex items-center justify-center'
+		>
+			{num}
+		</div>
+	</div>
 );
 const Navbar = () => {
-  const {
-    activeMenu,
-    setActiveMenu,
-    logout,
-    login,
-    screenSize,
-    setScreenSize,
-    darkToggle,
-    setDarkToggle,
-  } = useStateContext();
+	const {
+		activeMenu,
+		setActiveMenu,
+		logout,
+		login,
+		screenSize,
+		setScreenSize,
+		darkToggle,
+		setDarkToggle,
+	} = useStateContext();
 
-  const { data: session } = useSession();
-  const [dropDown, setDropDown] = useState(false);
-  const [notificationdropDown, setNotificationdropDown] = useState(false);
-  // const user = getUser()
+	const { data: session } = useSession();
+	const [dropDown, setDropDown] = useState(false);
+	const [notificationdropDown, setNotificationdropDown] = useState(false);
+	// const user = getUser()
 
-  const handleLogout = () => {
-    // removeUserSession();
-    // logout();
-    // navigate("/login");
-    // handleCloseSidebar();
-    // setActiveMenu(false);
-    console.log("loggingout");
-  };
+	const handleLogout = () => {
+		// removeUserSession();
+		// logout();
+		// navigate("/login");
+		// handleCloseSidebar();
+		// setActiveMenu(false);
+		console.log('loggingout');
+	};
 
-  const handleDropDown = () => {
-    if (dropDown) {
-      setDropDown(false);
-      setNotificationdropDown(false);
-    } else {
-      setDropDown(true);
-    }
-  };
-  const handleNotificationDropDown = () => {
-    setNotificationdropDown(true);
-    if (notificationdropDown) {
-      setDropDown(false);
-      setNotificationdropDown(false);
-    } else {
-      setNotificationdropDown(true);
-    }
-  };
+	const handleDropDown = () => {
+		if (dropDown) {
+			setDropDown(false);
+			setNotificationdropDown(false);
+		} else {
+			setDropDown(true);
+		}
+	};
+	const handleNotificationDropDown = () => {
+		setNotificationdropDown(true);
+		if (notificationdropDown) {
+			setDropDown(false);
+			setNotificationdropDown(false);
+		} else {
+			setNotificationdropDown(true);
+		}
+	};
 
-  useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth);
+	useEffect(() => {
+		const handleResize = () => setScreenSize(window.innerWidth);
 
-    window.addEventListener("resize", handleResize);
+		window.addEventListener('resize', handleResize);
 
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		handleResize();
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
-  useEffect(() => {
-    if (screenSize <= 900) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
-  }, [screenSize]);
+	useEffect(() => {
+		if (screenSize <= 900) {
+			setActiveMenu(false);
+		} else {
+			setActiveMenu(true);
+		}
+	}, [screenSize]);
 
-  return (
-    <div
-      className="flex justify-between p-2 relative bg-black text-white dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white"
-      //   style={{ borderBottom: "0.6px solid #878787" }}
-    >
-      <NavButton
-        title="Menu"
-        customFunc={() => setActiveMenu((prevState) => !prevState)}
-        color="blue"
-        icon={<AiOutlineMenu />}
-      />
+	return (
+		<div
+			className='flex justify-between p-2 relative bg-black text-white dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white'
+			//   style={{ borderBottom: "0.6px solid #878787" }}
+		>
+			<NavButton
+				title='Menu'
+				customFunc={() => setActiveMenu((prevState) => !prevState)}
+				color='blue'
+				icon={<AiOutlineMenu />}
+			/>
 
-      <div className="flex gap-x-[50px]">
-        <NavButton
-          title="Mode"
-          customFunc={() => setDarkToggle((prevState) => !prevState)}
-          color="white"
-          icon={<AiOutlineMail />}
-          dotColor="red"
-          num={4}
-        />
-        {/* <NavButton title='Chat' dotColor='#03c9D7' customFunc={() => handleClick('chat')} color='blue' icon={<BsChatLeft />} /> */}
+			<div className='flex gap-x-[50px]'>
+				<NavButton
+					title='Mode'
+					customFunc={() => setDarkToggle((prevState) => !prevState)}
+					color='white'
+					icon={<AiOutlineMail />}
+					dotColor='red'
+					num={4}
+				/>
+				{/* <NavButton title='Chat' dotColor='#03c9D7' customFunc={() => handleClick('chat')} color='blue' icon={<BsChatLeft />} /> */}
 
-        <div
-          className={`${
-            notificationdropDown ? "mt-[10px] md:mt-0" : "my-3 md:my-0"
-          } md:m-[30px]  relative flex items-center gap-2 hover:bg-light-gray rounded-lg  `}
-          // onMouseEnter={handleNotificationDropDown}
-          // onClick={() => setNotificationdropDown(!notificationdropDown)}
-          // onMouseLeave={() => setNotificationdropDown(false)}
-        >
-          <NavButton
-            title="Notifications"
-            dotColor="red"
-            customFunc={() => setNotificationdropDown(!notificationdropDown)}
-            color="white"
-            num={2}
-            icon={<RiNotification3Line />}
-          />
-          <ul
-            className={`${
-              notificationdropDown
-                ? "top-[45px] right-[2px] opacity-100 z-20 "
-                : "hidden"
-            } absolute  bg-white  font-normal text-xl w-[98vw] md:w-[384px] shadow-lg border rounded-lg overflow-y-auto dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white`}
-            style={{
-              zIndex: 20,
-              boxShadow:
-                "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px -1px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            <div className="flex w-full h-[40px] justify-center items-center bg-[#F9FAFB] rounded-lg dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white">
-              <p className="font-[500] text-[16px] leading-6 text-[#111928] dark:text-gray-200">
-                Notifications
-              </p>
-            </div>
-            {/* {data.map((notification) => (
+				<div
+					className={`${
+						notificationdropDown
+							? 'mt-[10px] md:mt-0'
+							: 'my-3 md:my-0'
+					} md:m-[30px]  relative flex items-center gap-2 hover:bg-light-gray rounded-lg  `}
+					// onMouseEnter={handleNotificationDropDown}
+					// onClick={() => setNotificationdropDown(!notificationdropDown)}
+					// onMouseLeave={() => setNotificationdropDown(false)}
+				>
+					<NavButton
+						title='Notifications'
+						dotColor='red'
+						customFunc={() =>
+							setNotificationdropDown(!notificationdropDown)
+						}
+						color='white'
+						num={2}
+						icon={<RiNotification3Line />}
+					/>
+					<ul
+						className={`${
+							notificationdropDown
+								? 'top-[45px] right-[2px] opacity-100 z-20 '
+								: 'hidden'
+						} absolute  bg-white  font-normal text-xl w-[98vw] md:w-[384px] shadow-lg border rounded-lg overflow-y-auto dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white`}
+						style={{
+							zIndex: 20,
+							boxShadow:
+								'0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px -1px rgba(0, 0, 0, 0.1)',
+						}}
+					>
+						<div className='flex w-full h-[40px] justify-center items-center bg-[#F9FAFB] rounded-lg dark:text-gray-200 dark:bg-main-dark-bg dark:hover:text-white'>
+							<p className='font-[500] text-[16px] leading-6 text-[#111928] dark:text-gray-200'>
+								Notifications
+							</p>
+						</div>
+						{/* {data.map((notification) => (
               <li className="mb-3 flex items-center border-b border-b-[#F9FAFB] pb-[15px] py-4  px-4">
                 <img
                   src={notification.image}
@@ -163,32 +166,32 @@ const Navbar = () => {
                 </div>
               </li>
             ))} */}
-            <div className="flex w-full h-[40px] justify-center items-center bg-[#F9FAFB] rounded-lg">
-              <IoEyeSharp color="#6B7280" />
-              <p className="font-[500] text-[16px] leading-6 text-[#111928] ml-[8px]">
-                View All
-              </p>
-            </div>
-          </ul>
-        </div>
-        <div
-          className={`${
-            dropDown ? "mt-[10px] md:mt-0" : "my-3 md:my-0"
-          } md:mr-[30px]  relative flex items-center gap-2 cursor-pointer hover:bg-light-gray rounded-lg `}
-          onMouseEnter={handleDropDown}
-          onClick={() => setDropDown(!dropDown)}
-          onMouseLeave={() => setDropDown(false)}
-          // onClick={() => handleClick("userProfile")}
-        >
-          <p className="text-gray-400 text-[10px] md:text-14 font-bold ml-1">
-            Tosin
-          </p>
-          <img
-            src="/images/instructor/alalade.png"
-            alt="avatar"
-            className="rounded-full w-8 h-8 ml-4"
-          />
-          {/* <MdKeyboardArrowDown className='text-gray-400 text-14 mr-3' />
+						<div className='flex w-full h-[40px] justify-center items-center bg-[#F9FAFB] rounded-lg'>
+							<IoEyeSharp color='#6B7280' />
+							<p className='font-[500] text-[16px] leading-6 text-[#111928] ml-[8px]'>
+								View All
+							</p>
+						</div>
+					</ul>
+				</div>
+				<div
+					className={`${
+						dropDown ? 'mt-[10px] md:mt-0' : 'my-3 md:my-0'
+					} md:mr-[30px]  relative flex items-center gap-2 cursor-pointer hover:bg-light-gray rounded-lg `}
+					onMouseEnter={handleDropDown}
+					onClick={() => setDropDown(!dropDown)}
+					onMouseLeave={() => setDropDown(false)}
+					// onClick={() => handleClick("userProfile")}
+				>
+					<p className='text-gray-400 text-[10px] md:text-14 font-bold ml-1'>
+						Tosin
+					</p>
+					<img
+						src='/images/instructor/alalade.png'
+						alt='avatar'
+						className='rounded-full w-8 h-8 ml-4'
+					/>
+					{/* <MdKeyboardArrowDown className='text-gray-400 text-14 mr-3' />
           <ul
             className={`${
               dropDown ? 'top-[40px] opacity-100 pb-6 z-20 ' : 'hidden'
@@ -208,14 +211,14 @@ const Navbar = () => {
               </button>
             </li>
           </ul> */}
-        </div>
-        {/* { isClicked.cart && <Cart /> }
+				</div>
+				{/* { isClicked.cart && <Cart /> }
         { isClicked.chat && <Chat /> }
         { isClicked.notification && <Notification /> }
         { isClicked.userProfile && <UserProfile /> } */}
-      </div>
-    </div>
-  );
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -1,98 +1,109 @@
-import React from "react";
-import { SiShopware } from "react-icons/si";
-import { MdOutlineCancel } from "react-icons/md";
-import { FiLogOut, FiSettings } from "react-icons/fi";
-import { links } from "../../links";
-import Link from "next/link";
-import { useStateContext } from "../../AuthContext";
-import { signOut } from "next-auth/react";
+import React from 'react';
+import { SiShopware } from 'react-icons/si';
+import { MdOutlineCancel } from 'react-icons/md';
+import { FiLogOut, FiSettings } from 'react-icons/fi';
+import { links } from '../../links';
+import Link from 'next/link';
+import { useStateContext } from '../../AuthContext';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 const Sidebar = () => {
-  let { activeMenu, setActiveMenu, screenSize, logout, login } =
-    useStateContext();
+	let { activeMenu, setActiveMenu, screenSize, logout, login, user } =
+		useStateContext();
+	const router = useRouter();
+	const handleLogout = async () => {
+		const data = await signOut({ redirect: false, callbackUrl: '/' });
+		router.push(data.url);
+	};
+	const handleCloseSidebar = () => {
+		if (activeMenu && screenSize <= 900) {
+			setActiveMenu(false);
+		}
+	};
+	const activeLink =
+		'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg bg-blue-500 text-white text-md m-2';
+	const normalLink =
+		'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-[#8C8D8E] font-[400] text-[16px] dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
+	return (
+		<div className='h-screen overflow-y-auto md:overflow-x-hidden  md:hover:overflow-y-auto pb-[10px] w-[218px] bg-[#101010]'>
+			{activeMenu && (
+				<>
+					<div className='flex justify-between items-center'>
+						<Link
+							href='/'
+							onClick={handleCloseSidebar}
+							className='items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900'
+						>
+							<img
+								src='/images/logo.png'
+								alt='Devcent Logo'
+								className='max-w-[200px]'
+							/>
+						</Link>
+						<button
+							type='button'
+							onClick={() =>
+								setActiveMenu((prevState) => !prevState)
+							}
+							className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
+						>
+							<MdOutlineCancel />
+						</button>
+					</div>
+					<div className='mt-5 w-[225px]'>
+						{user?.enrolledCourses.length >= 1 &&
+							links.map((item) => (
+								<div key={item.title}>
+									{/* <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p> */}
 
-  const handleCloseSidebar = () => {
-    if (activeMenu && screenSize <= 900) {
-      setActiveMenu(false);
-    }
-  };
-  // private $servername = "ftp.alfaahizuunschools.com";
-  const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg bg-blue-500 text-white text-md m-2";
-  const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-[#8C8D8E] font-[400] text-[16px] dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
-  return (
-    <div className="h-screen overflow-y-auto md:overflow-x-hidden  md:hover:overflow-y-auto pb-[10px] w-[218px] bg-[#101010]">
-      {activeMenu && (
-        <>
-          <div className="flex justify-between items-center">
-            <Link
-              href="/overview"
-              onClick={handleCloseSidebar}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-            >
-              <img
-                src="/images/logo.png"
-                alt="Devcent Logo"
-                className="max-w-[200px]"
-              />
-              {/* <span>Prompay</span> */}
-            </Link>
-            <button
-              type="button"
-              onClick={() => setActiveMenu((prevState) => !prevState)}
-              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
-            >
-              <MdOutlineCancel />
-            </button>
-          </div>
-          <div className="mt-5 w-[225px]">
-            {links.map((item) => (
-              <div key={item.title}>
-                {/* <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p> */}
-
-                {item.links.map((link) => {
-                  return (
-                    <Link
-                      href={`/auth/${link.name}`}
-                      key={link.name}
-                      onClick={handleCloseSidebar}
-                      className={activeMenu ? normalLink : activeLink}
-                    >
-                      {link.icon}
-                      <span className="capitalize font-space font-[500] text-[14px] ">
-                        {/* {link.name == '/' ? 'overview' : link.name} */}
-                        {link.name}
-                      </span>
-                    </Link>
-                  );
-                })}
-              </div>
-            ))}
-          </div>
-        </>
-      )}
-      <button
-        onClick={() => signOut()}
-        className="flex items-center w-[90%] gap-x-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md  mt-[40px]"
-      >
-        <FiSettings />
-        <span className="capitalize">Settings</span>
-      </button>
-      <button
-        onClick={() => {
-          signOut({
-            redirect: true,
-            callbackUrl: "/",
-          });
-        }}
-        className="flex items-center w-[90%] gap-x-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md "
-      >
-        <FiLogOut />
-        <span className="capitalize">logout</span>
-      </button>
-    </div>
-  );
+									{item.links.map((link) => {
+										return (
+											<Link
+												href={`/auth/${link.name}`}
+												key={link.name}
+												onClick={handleCloseSidebar}
+												className={
+													activeMenu
+														? normalLink
+														: activeLink
+												}
+											>
+												{link.icon}
+												<span className='capitalize font-space font-[500] text-[14px] '>
+													{/* {link.name == '/' ? 'overview' : link.name} */}
+													{link.name}
+												</span>
+											</Link>
+										);
+									})}
+								</div>
+							))}
+					</div>
+				</>
+			)}
+			<button
+				onClick={() => signOut()}
+				className='flex items-center w-[90%] gap-x-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md  mt-[40px]'
+			>
+				<FiSettings />
+				<span className='capitalize'>Settings</span>
+			</button>
+			<button
+				// onClick={() => {
+				// 	signOut({
+				// 		redirect: true,
+				// 		callbackUrl: '/',
+				// 	});
+				// }}
+				onClick={handleLogout}
+				className='flex items-center w-[90%] gap-x-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md '
+			>
+				<FiLogOut />
+				<span className='capitalize'>logout</span>
+			</button>
+		</div>
+	);
 };
 
 export default Sidebar;
