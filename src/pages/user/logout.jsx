cdc369b7c453891d52dfaaba370984d6189df513
femmyte <sanyaoluadefemi@gmail.com>
2023-06-17 14:00:04 +0100

    Created the login page that remove user session

---
+++ src/pages/user/logout.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import DashboardLayout from '../../../components/dashboard/DashboardLayout';
+
+const Logout = () => {
+	const router = useRouter();
+	const handleLogout = async () => {
+		const data = await signOut({ redirect: false, callbackUrl: '/' });
+		router.push(data.url);
+	};
+	return (
+		<>
+			<DashboardLayout>
+				<div className='px-[20px]'>
+					<p className='font-space font-[700] text-[24px] leading-[30.2px]  mb-[40px]'>
+						Logout Account
+					</p>
+
+					<p className='mt-[25px] font-dmsans font-[600] text-[20px] leading-[20.2px] text-white'>
+						Are you sure you want to logout your account?
+					</p>
+					<div className='flex gap-x-[60px] mt-[50px]'>
+						<button
+							type='submit'
+							className=' py-[8px] px-[16px] rounded-lg bg-primaryPurple font-dmsans font-[500] text-[24px] hover:bg-primaryYellow'
+						>
+							Don’t logout
+						</button>
+						<button
+							type='submit'
+							className=' py-[8px] px-[16px] rounded-lg border border-primaryPurple hover:bg-primaryPurple font-dmsans font-[500] text-[24px]'
+							onClick={handleLogout}
+						>
+							Logout my account
+						</button>
+					</div>
+				</div>
+			</DashboardLayout>
+		</>
+	);
+};
+
+export default Logout;
