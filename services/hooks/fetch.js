96d04cdcbc7509c62c1fc918a3ef0a9433753ee2
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2023-06-20 11:11:17 +0100

    changes in profile form

--- services/hooks/fetch.js
+++ services/hooks/fetch.js
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import httpService from "services/httpService";
 
-export function useFetch(url, key, query = {}) {
+export function useFetch(
+  url,
+  key,
+  query = {},
+  options = { refetchOnWindowFocus: true }
+) {
   const fetchData = async () => {
     const { data } = await httpService.get(url, {
       params: { ...query },
@@ -9,5 +14,7 @@ export function useFetch(url, key, query = {}) {
 
     return data;
   };
-  return useQuery([key, query], () => fetchData());
+  return useQuery([key, query], () => fetchData(), {
+    ...options,
+  });
 }
--- src/pages/user/settings/profile.jsx
+++ src/pages/user/settings/profile.jsx
@@ -1,266 +1,184 @@
-import React, { useEffect, useState } from 'react';
-import DashboardLayout from '../../../../components/dashboard/DashboardLayout';
-import courseInfo from '../../../../courseInfo';
-import { getSession, useSession } from 'next-auth/react';
-import { useStateContext } from 'AuthContext';
-import { useRouter } from 'next/router';
-import { useFetch } from 'services/hooks/fetch';
-import FullLoader from 'components/loaders/FullLoader';
-import FullError from 'components/error/FullError';
-import { enrolInCourse } from 'services/paymentService';
-import Link from 'next/link';
-import ButtonLoader from 'components/loaders/ButtonLoader';
-import Image from 'next/image';
-import DateInput from 'components/common/DateSelector';
+import React, { useEffect, useState } from "react";
+import DashboardLayout from "../../../../components/dashboard/DashboardLayout";
+import courseInfo from "../../../../courseInfo";
+import { getSession, useSession } from "next-auth/react";
+import { useStateContext } from "AuthContext";
+import { useRouter } from "next/router";
+import { useFetch } from "services/hooks/fetch";
+import FullLoader from "components/loaders/FullLoader";
+import FullError from "components/error/FullError";
+import { enrolInCourse } from "services/paymentService";
+import Link from "next/link";
+import ButtonLoader from "components/loaders/ButtonLoader";
+import Image from "next/image";
+import DateInput from "components/common/DateSelector";
+
 const Studentprofile = () => {
-	const { user } = useStateContext();
-	const [courseInformation, setCourseInformationo] = useState(courseInfo);
-	// const fetchUser = async () => {
-	// 	try {
-	// 		const response = await fetch('students');
-	// 		const data = await response.json();
-	// 		setFetchedUsers(data);
-	// 		data?.map((user) => setCourse(user.course));
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 	}
-	// };
-	// useEffect(() => {
-	// 	fetchUser();
-	// }, []);
+  const { user } = useStateContext();
 
-	useEffect(() => {
-		getSession().then((session) => {
-			// console.log(session);
-		});
-	}, []);
+  const [profile, setProfile] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    bio: "",
+    day: "",
+    month: "",
+    year: "",
+  });
 
-	const router = useRouter();
+  const handleChange = (evt) => {
+    console.log("change");
+    const value = evt.target.value;
+    console.log(value);
+    setProfile({
+      ...profile,
+      [evt.target.name]: value,
+    });
+  };
 
-	// const {
-	// 	data,
-	// 	isInitialLoading,
-	// 	isSuccess,
-	// 	isError: isErrorFetching,
-	// 	refetch,
-	// } = useFetch(`/courses/${courseName}/course`, 'get-course');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-	// useEffect(() => {
-	// 	if (isSuccess) {
-	// 		setCourse(data.course);
-	// 	}
+    if (
+      !profile.firstName ||
+      !profile.lastName ||
+      !profile.phoneNumber ||
+      !profile.bio ||
+      !profile.day ||
+      !profile.month ||
+      !profile.year
+    ) {
+      setErrorEnrol("Fields with * are required");
+      return;
+    }
+  };
 
-	// 	refetch();
-	// }, [courseName, isSuccess, data, refetch]);
-
-	const [state, setState] = useState({
-		paymentPlan: '',
-		firstName: '',
-		lastName: '',
-		email: '',
-		country: '',
-		address: '',
-		province: '',
-		city: '',
-		postalCode: '',
-		phoneNumber: '',
-		agreement: false,
-	});
-	const [errorEnrol, setErrorEnrol] = useState('');
-	const [isLoadingEnrol, setIsLoadingEnrol] = useState(false);
-
-	const handleChange = (evt) => {
-		const value =
-			evt.target.type === 'checkbox'
-				? evt.target.checked
-				: evt.target.value;
-		setState({
-			...state,
-			[evt.target.name]: value,
-		});
-	};
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setErrorEnrol('');
-
-		if (
-			!state.paymentPlan ||
-			!state.firstName ||
-			!state.lastName ||
-			!state.email ||
-			!state.country ||
-			!state.address ||
-			!state.province ||
-			!state.city ||
-			!state.postalCode ||
-			!state.phoneNumber ||
-			!state.agreement
-		) {
-			setErrorEnrol('Fields with * are required');
-			return;
-		}
-		// setIsLoadingEnrol(true);
-
-		// try {
-		// 	const { data } = await enrolInCourse(
-		// 		`/courses/${courseName}/enrol`,
-		// 		{
-		// 			courseId: course.courseId,
-		// 			paymentPlan: state.paymentPlan,
-		// 			firstName: state.firstName,
-		// 			lastName: state.lastName,
-		// 			email: state.email,
-		// 			country: state.country,
-		// 			city: state.city,
-		// 			address: state.address,
-		// 			province: state.province,
-		// 			postalCode: state.postalCode,
-		// 			phoneNumber: state.phoneNumber,
-		// 		},
-		// 		session?.data?.accessToken
-		// 	);
-
-		// 	router.push(data.paymentPage);
-		// } catch (err) {
-		// 	if (err?.response?.data) {
-		// 		setErrorEnrol(err.response.data.message);
-		// 	}
-		// } finally {
-		// 	setIsLoadingEnrol(false);
-		// }
-	};
-
-	// if (isInitialLoading) {
-	// 	return <FullLoader />;
-	// }
-	// if (isErrorFetching) {
-	// 	return <FullError />;
-	// }
-	return (
-		<>
-			<DashboardLayout>
-				<div className='px-[20px]'>
-					<div className='flex flex-col gap-y-[20px] md:flex-row justify-between items-center'>
-						<div className='flex flex-col gap-y-[20px] md:flex-row gap-x-[63px] items-center'>
-							<Image
-								src='/images/profileImg.png'
-								height={100}
-								width={100}
-								alt='profile Image'
-							/>
-							<button
-								type='submit'
-								className=' py-[8px] px-[16px] rounded-lg bg-primaryPurple font-dmsans font-[500] text-[24px] hover:bg-primaryYellow'
-							>
-								Upload New
-							</button>
-							<button
-								type='submit'
-								className=' py-[8px] px-[16px] rounded-lg border border-primaryPurple hover:bg-primaryPurple font-dmsans font-[500] text-[24px]'
-							>
-								Delete Photo
-							</button>
-						</div>
-						<p className='font-space font-[700] text-[18px] leading-[22.97.2px]'>
-							Student ID-123456
-						</p>
-					</div>
-					<p className='mt-[30px] md:mt-[66px] font-source font-[700] text-[28px] md:text-[32px] leading-[40.2px]  mb-[24px]'>
-						Personal Details
-					</p>
-					{/* <div className='md:flex gap-x-8 justify-between  mb-[24px]'> */}
-					<div className='flex flex-col flex-1 mb-[24px]'>
-						<label
-							htmlFor='fname'
-							className='font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]'
-						>
-							First Name <span className='text-red-600'>*</span>
-						</label>
-						<input
-							type='text'
-							className='p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg'
-							placeholder='John'
-							id='fname'
-							name='firstName'
-							onChange={handleChange}
-							value={state.firstName}
-							required
-						/>
-					</div>
-					<div className='flex flex-col flex-1 mb-[24px]'>
-						<label
-							htmlFor='lname'
-							className='font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]'
-						>
-							Last Name <span className='text-red-600'>*</span>
-						</label>
-						<input
-							type='text'
-							className='p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent w-[100%] rounded-lg'
-							placeholder='Doe'
-							id='lname'
-							name='lastName'
-							onChange={handleChange}
-							value={state.lastName}
-							required
-						/>
-					</div>
-					<DateInput />
-					<div className='flex flex-col  mb-[24px]'>
-						<label
-							htmlFor='phone'
-							className='font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]'
-						>
-							Phone Number <span className='text-red-600'>*</span>
-						</label>
-						<input
-							type='tel'
-							className='p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg'
-							placeholder='8178627581'
-							id='phone'
-							name='phoneNumber'
-							onChange={handleChange}
-							value={state.phoneNumber}
-							required
-						/>
-					</div>
-					<div className='flex flex-col  mb-[24px]'>
-						<label
-							htmlFor='phone'
-							className='font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]'
-						>
-							About Me <span className='text-red-600'>*</span>
-						</label>
-						<textarea
-							type='text'
-							className='p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg h-[200px]'
-							placeholder='Tell us a bit about yourself, the things that interest you and what you hope to gain from thi course'
-							id='phone'
-							name='phoneNumber'
-							onChange={handleChange}
-							// value={state.about}
-							required
-						></textarea>
-					</div>
-					<p className='mt-[30px] font-dmsans font-[700] text-[14px] leading-[17px]  mb-[24px] md:w-[599px]'>
-						Please note that post you make in your discussion groups
-						would display your name, image and student ID. To read
-						full extent, check our our{' '}
-						<Link href='#' className='text-[#0A66C2]'>
-							Terms and conditions
-						</Link>
-					</p>
-					<button
-						type='submit'
-						className=' py-[8px] px-[16px] rounded-lg bg-primaryPurple font-dmsans font-[500] text-[24px] hover:bg-primaryYellow'
-					>
-						Save Changes
-					</button>
-				</div>
-			</DashboardLayout>
-		</>
-	);
+  return (
+    <>
+      <DashboardLayout>
+        <div className="px-[20px]">
+          <div className="flex flex-col gap-y-[20px] md:flex-row justify-between items-center">
+            <div className="flex flex-col gap-y-[20px] md:flex-row gap-x-[63px] items-center">
+              <Image
+                src="/images/profileImg.png"
+                height={100}
+                width={100}
+                alt="profile Image"
+              />
+              <button
+                type="submit"
+                className=" py-[8px] px-[16px] rounded-lg bg-primaryPurple font-dmsans font-[500] text-[24px] hover:bg-primaryYellow"
+              >
+                Upload New
+              </button>
+              <button
+                type="submit"
+                className=" py-[8px] px-[16px] rounded-lg border border-primaryPurple hover:bg-primaryPurple font-dmsans font-[500] text-[24px]"
+              >
+                Delete Photo
+              </button>
+            </div>
+            <p className="font-space font-[700] text-[18px] leading-[22.97.2px]">
+              Student ID-123456
+            </p>
+          </div>
+          <p className="mt-[30px] md:mt-[66px] font-source font-[700] text-[28px] md:text-[32px] leading-[40.2px]  mb-[24px]">
+            Personal Details
+          </p>
+          {/* <div className='md:flex gap-x-8 justify-between  mb-[24px]'> */}
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col flex-1 mb-[24px]">
+              <label
+                htmlFor="first-name"
+                className="font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]"
+              >
+                First Name <span className="text-red-600">*</span>
+              </label>
+              <input
+                type="text"
+                className="p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg"
+                placeholder="John"
+                id="first-name"
+                name="firstName"
+                onChange={handleChange}
+                value={profile.firstName}
+                required
+              />
+            </div>
+            <div className="flex flex-col flex-1 mb-[24px]">
+              <label
+                htmlFor="last-name"
+                className="font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]"
+              >
+                Last Name <span className="text-red-600">*</span>
+              </label>
+              <input
+                type="text"
+                className="p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent w-[100%] rounded-lg"
+                placeholder="Doe"
+                id="last-name"
+                name="lastName"
+                onChange={handleChange}
+                value={profile.lastName}
+                required
+              />
+            </div>
+            <DateInput profile={profile} handleChange={handleChange} />
+            <div className="flex flex-col  mb-[24px]">
+              <label
+                htmlFor="phone"
+                className="font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]"
+              >
+                Phone Number <span className="text-red-600">*</span>
+              </label>
+              <input
+                type="tel"
+                className="p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg"
+                placeholder="08178627581"
+                id="phone"
+                name="phoneNumber"
+                onChange={handleChange}
+                value={profile.phoneNumber}
+                required
+              />
+            </div>
+            <div className="flex flex-col  mb-[24px]">
+              <label
+                htmlFor="bio"
+                className="font-source font-[600] text-[24px] leading-[30px] text-[#9b9b9b] mb-[16px]"
+              >
+                About Me <span className="text-red-600">*</span>
+              </label>
+              <textarea
+                type="text"
+                className="p-4 border border-[#747474] text-[#747474] font-[600] text-[16px] leading-5 bg-transparent rounded-lg h-[200px]"
+                placeholder="Tell us a bit about yourself, the things that interest you and what you hope to gain from thi course"
+                id="bio"
+                name="bio"
+                onChange={handleChange}
+                value={profile.bio}
+                required
+              ></textarea>
+            </div>
+            <p className="mt-[30px] font-dmsans font-[700] text-[14px] leading-[17px]  mb-[24px] md:w-[599px]">
+              Please note that post you make in your discussion groups would
+              display your name, image and student ID. To read full extent,
+              check our our{" "}
+              <Link href="#" className="text-[#0A66C2]">
+                Terms and conditions
+              </Link>
+            </p>
+            <button
+              type="submit"
+              className=" py-[8px] px-[16px] rounded-lg bg-primaryPurple font-dmsans font-[500] text-[24px] hover:bg-primaryYellow"
+            >
+              Save Changes
+            </button>
+          </form>
+        </div>
+      </DashboardLayout>
+    </>
+  );
 };
 
 export default Studentprofile;
