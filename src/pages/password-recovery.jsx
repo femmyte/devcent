c4b951b05a840bb28200014b3171d0b40987f664
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2023-06-26 21:41:55 +0100

    finalized password recovery

--- src/pages/password-recovery.jsx
+++ src/pages/password-recovery.jsx
@@ -1,4 +1,6 @@
 import withLogoutAuth from "components/auth/withLogoutAuth";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 import httpService from "services/httpService";
@@ -7,9 +9,11 @@ const RecoveryPage = () => {
   const [email, setEmail] = useState("");
   const [recoveryCode, setRecoveryCode] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [step, setStep] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [accessToken, setAccessToken] = useState("");
 
   const handleEmailSubmit = async (e) => {
     e.preventDefault();
@@ -31,16 +35,55 @@ const RecoveryPage = () => {
     }
   };
 
-  const handleRecoveryCodeSubmit = (e) => {
+  const handleRecoveryCodeSubmit = async (e) => {
     e.preventDefault();
-    // TODO: Handle verification code submission and validation
-    setStep(3);
+    setError("");
+    setIsLoading(true);
+
+    try {
+      const { data } = await httpService.post(
+        "/users/password/verify-recovery-token",
+        {
+          token: recoveryCode,
+        }
+      );
+      toast(data.message);
+      setAccessToken(data.accessToken);
+      setStep(3);
+    } catch (error) {
+      if (error?.response?.data?.message) {
+        setError(error.response.data.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const handlePasswordSubmit = (e) => {
+  const handlePasswordReset = async (e) => {
     e.preventDefault();
-    // TODO: Handle password submission and reset
-    setStep(4);
+    setError("");
+
+    if (newPassword !== confirmPassword) {
+      setError("Passwords must match");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const { data } = await httpService.post("/users/password/reset", {
+        password: newPassword,
+        accessToken,
+      });
+      toast(data.message);
+      setStep(4);
+    } catch (error) {
+      if (error?.response?.data?.message) {
+        setError(error.response.data.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const renderStepContent = () => {
@@ -58,7 +101,7 @@ const RecoveryPage = () => {
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="rounded-md p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
+                className="shadow-sm rounded-md p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
               />
               <p className="min-h-[30px] my-2 pl-2 text-red-500 ">{error}</p>
               <button
@@ -84,7 +127,7 @@ const RecoveryPage = () => {
                 placeholder="Enter the recovery code"
                 value={recoveryCode}
                 onChange={(e) => setRecoveryCode(e.target.value)}
-                className="rounded-md p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
+                className="shadow-sm rounded-md p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
               />
               <p className="min-h-[30px] my-2 pl-2 text-red-500 ">{error}</p>
               <button
@@ -102,19 +145,28 @@ const RecoveryPage = () => {
           <>
             <h2 className="text-2xl font-bold mb-4">Reset Password</h2>
             <p className="mb-4">Please enter your new password.</p>
-            <form onSubmit={handlePasswordSubmit}>
+            <form onSubmit={handlePasswordReset}>
               <input
                 type="password"
-                placeholder="Enter your new password"
+                placeholder="Enter password"
                 value={newPassword}
                 onChange={(e) => setNewPassword(e.target.value)}
-                className="rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
+                className="shadow-sm rounded-md mb-4 p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
               />
+              <input
+                type="password"
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="shadow-sm rounded-md p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 w-full"
+              />
+              <p className="min-h-[30px] my-2 pl-2 text-red-500 ">{error}</p>
               <button
                 type="submit"
-                className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+                disabled={isLoading}
+                className="mt-2 px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
               >
-                Reset Password
+                {isLoading ? "Loading..." : "Reset Password"}
               </button>
             </form>
           </>
@@ -129,6 +181,9 @@ const RecoveryPage = () => {
               Password reset successful! You can now log in with your new
               password.
             </p>
+            <button className="mt-4 px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700">
+              <Link href="/signin">Log in</Link>
+            </button>
           </>
         );
       default:
@@ -137,9 +192,18 @@ const RecoveryPage = () => {
   };
 
   return (
-    <div className="w-full h-screen bg-black flex items-center">
-      <div className="text-black w-[92%] sm:w-[448px] mx-auto p-4 bg-white shadow-md rounded-md">
-        {renderStepContent()}
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <div>
+        <div className="flex gap-4 mb-6 text-white items-center justify-center px-4">
+          <Link href={"/"}>
+            <Image src="/images/logo.png" width={100} height={100} alt="logo" />
+          </Link>
+
+          <h1 className="text-[1.7rem]">Trainings</h1>
+        </div>
+        <div className="text-black w-[92%] sm:w-[448px] mx-auto p-4 bg-white shadow-md rounded-md">
+          {renderStepContent()}
+        </div>
       </div>
     </div>
   );
